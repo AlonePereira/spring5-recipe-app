75e9471724cc3807046a4c61539586a8ca44198a
Alone Silva <alone.silva@reservafacil.tur.br>
2021-05-17 06:42:03 -0300

    Create test with Mockito

--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -1,11 +1,39 @@
 package guru.springframework.services;
 
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.Set;
 
 import static org.junit.Assert.*;
+
 public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
 
     @Test
     public void getRecipes() {
+
+        Mockito.when(recipeRepository.findAll()).thenReturn(Arrays.asList(new Recipe()));
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(1, recipes.size());
     }
 }
