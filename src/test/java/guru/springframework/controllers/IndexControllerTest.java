bddba679de5c9bd3cfa0395a9cbcab08426dfe5f
Alone Silva <alone.silva@reservafacil.tur.br>
2021-05-17 07:58:08 -0300

    Mock with ArgumentCaptor

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -4,6 +4,7 @@ import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -37,11 +38,18 @@ public class IndexControllerTest {
         Set<Recipe> recipes = new HashSet<>();
         recipes.add(new Recipe());
 
+        Recipe recipe = new Recipe();
+        recipe.setId(4L);
+        recipes.add(recipe);
+
         Mockito.when(recipeService.getRecipes()).thenReturn(recipes);
         String indexPage = indexController.getIndexPage(model);
 
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
         assertEquals("index", indexPage);
         Mockito.verify(recipeService, Mockito.times(1)).getRecipes();
-        Mockito.verify(model, Mockito.times(1)).addAttribute(Mockito.anyString(), Mockito.any());
+        Mockito.verify(model, Mockito.times(1)).addAttribute(Mockito.anyString(), argumentCaptor.capture());
+        assertEquals(2, argumentCaptor.getValue().size());
     }
 }
