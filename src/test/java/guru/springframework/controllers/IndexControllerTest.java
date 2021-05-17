198b10cd582cf35d34c736a28f5f6433c6d1c2ec
Alone Silva <alone.silva@reservafacil.tur.br>
2021-05-17 06:58:30 -0300

    Create test for IndexController

---
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,47 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class IndexControllerTest {
+
+    IndexController indexController;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Mockito.when(recipeService.getRecipes()).thenReturn(recipes);
+        String indexPage = indexController.getIndexPage(model);
+
+        assertEquals("index", indexPage);
+        Mockito.verify(recipeService, Mockito.times(1)).getRecipes();
+        Mockito.verify(model, Mockito.times(1)).addAttribute(Mockito.anyString(), Mockito.any());
+    }
+}
