5ccac249c6282c6da351ea5efba026f7e49d82a7
Alone Silva <alone.silva@reservafacil.tur.br>
2021-05-31 08:29:36 -0300

    Adding Update Recipe

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -19,7 +19,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
@@ -33,12 +33,19 @@ public class RecipeController {
         return "recipe/recipeform";
     }
 
+    @RequestMapping("/recipe/{id}/update")
+    public String updateRecipe(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+
+        return "recipe/recipeform";
+    }
+
     @PostMapping("/recipe")
     public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
         command.setDifficulty(Difficulty.EASY);
         RecipeCommand saveRecipeCommand = recipeService.saveRecipeCommand(command);
 
-        return "redirect:/recipe/show/" + saveRecipeCommand.getId();
+        return "redirect:/recipe/" + saveRecipeCommand.getId() + "/show";
     }
 
 }
